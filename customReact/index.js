0245fcc2610ad85549e980cd9fd04b99ed95cc50
Devender Kumar Thakur <101546402+DevenderKThakur@users.noreply.github.com>
2024-11-17 10:10:57 +0530

    Add files via upload

---
+++ customReact/index.js
@@ -0,0 +1,25 @@
+function customRender(element , container){
+    const domElement = document.createElement(element.type)
+    domElement.innerHTML = element.children
+    for (const prop in element.props) {
+        if (prop === 'children') {
+            continue
+        }
+        domElement.setAttribute(prop , element.props[prop])
+    }
+    container.appendChild(domElement)
+}
+
+const reactElement = {
+    type : 'a' , 
+    props : {
+        href : 'https://google.com' , 
+        target : '_blank' 
+    },
+    children : 'Click me to visit google'
+}
+
+const root = document.getElementById("root")
+
+customRender (reactElement , root) 
+
